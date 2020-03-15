aefde5474f205dacbea8b0776f77a9dc41a1854e
Nebulis <nebounet@gmail.com>
2020-03-15 15:50:29 +0800

    feat: add details to recipe card

--- src/HomePage/Home.tsx
+++ src/HomePage/Home.tsx
@@ -1,24 +1,72 @@
 import React, { useEffect, useState } from "react";
 import { database, RECIPES_COLLECTION } from "../firebase/configuration";
+import { Clock, Oven, Pause, User, Bolt } from "../icon";
+import { Link } from "react-router-dom";
 
 interface Recipe {
   id: string;
   name: string;
   imageUrl: string;
   categories: string[];
+  cookTime: number;
+  prepareTime: number;
+  restTime: number;
+  serves: number;
+  calories: number;
+  createdAt: any;
+  steps: string[];
 }
+
+const transformTime = (time: number) => {
+  return time ? `${time} mins` : "-";
+};
 
 interface RecipeCardProps extends Recipe {
   name: string;
 }
-const RecipeCard: React.FunctionComponent<RecipeCardProps> = ({ name, categories, imageUrl }) => {
+const RecipeCard: React.FunctionComponent<RecipeCardProps> = ({
+  id,
+  name,
+  categories,
+  imageUrl,
+  serves,
+  prepareTime,
+  cookTime,
+  restTime,
+  calories
+}) => {
   return (
-    <div className="mx-auto max-w-sm rounded overflow-hidden shadow-lg">
+    <Link
+      to={`/recipe/${id}`}
+      className="mx-auto max-w-sm rounded overflow-hidden shadow-lg hover:shadow-2xl transition duration-150 ease-in transform hover:scale-105 flex flex-col"
+    >
       <img className="w-full" src={imageUrl} alt="Sunset in the mountains" />
       <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2">{name}</div>
+        <div className="font-bold text-xl mb-2 text-center">{name}</div>
       </div>
-      <div className="px-6 py-4">
+      <div className="flex mt-auto">
+        <div className="w-1/5 flex flex-col items-center">
+          <User className="fill-current w-4 h-4" />
+          {serves}
+        </div>
+        <div className="w-1/5 flex flex-col items-center">
+          <Clock className="fill-current w-4 h-4" />
+          {transformTime(prepareTime)}
+        </div>
+        <div className="w-1/5 flex flex-col items-center">
+          <Oven className="fill-current w-4 h-4" />
+          {transformTime(cookTime)}
+        </div>
+        <div className="w-1/5 flex flex-col items-center">
+          <Pause className="fill-current w-4 h-4" />
+          {transformTime(restTime)}
+        </div>
+        <div className="w-1/5 flex flex-col items-center">
+          <Bolt className="fill-current w-4 h-4" />
+          {calories}
+        </div>
+      </div>
+      <div className="px-6 py-4 text-center">
         {categories.map(category => {
           return (
             <span
@@ -30,7 +78,7 @@ const RecipeCard: React.FunctionComponent<RecipeCardProps> = ({ name, categories
           );
         })}
       </div>
-    </div>
+    </Link>
   );
 };
 
